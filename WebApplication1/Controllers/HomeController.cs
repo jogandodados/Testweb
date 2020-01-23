005342bd0012349cfad459a103b9acff5ee0bd48
jogandodados <pedro.robertofiori@gmail.com>
2020-01-23 09:02:21 -0300

    First Model -Controller-View-Department

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using WebApplication1.Models;
+using WebApplication1.Models.ViewModels;
 
 namespace WebApplication1.Controllers
 {
